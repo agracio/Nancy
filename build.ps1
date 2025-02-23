7ab2a8ecf4fffb21362c3740ac187c3c3c9055bb
agracio <agracio@users.noreply.github.com>
2025-02-23 22:37:45 +0000

    updating CI

--- build.ps1
+++ build.ps1
@@ -1,8 +1,4 @@
-$CakeVersion = "0.24.0"
-$DotNetVersion = select-string -Path .\tools\global.json -Pattern '[\d]\.[\d]\.[\d]' | % {$_.Matches} | % {$_.Value };
-# $DotNetVersion = "8.0.406";
-Write-Host "DotNetVersion: $DotNetVersion"
-$DotNetInstallerUri = "https://raw.githubusercontent.com/dotnet/cli/v$DotNetVersion/scripts/obtain/dotnet-install.ps1";
+$CakeVersion = "1.3.0"
 
 # Make sure tools folder exists
 $PSScriptRoot = $pwd
@@ -12,50 +8,6 @@ if (!(Test-Path $ToolPath)) {
     Write-Verbose "Creating tools directory..."
     New-Item -Path $ToolPath -Type directory | out-null
 }
-
-###########################################################################
-# INSTALL .NET CORE CLI
-###########################################################################
-
-# Function Remove-PathVariable([string]$VariableToRemove)
-# {
-#     $path = [Environment]::GetEnvironmentVariable("PATH", "User")
-#     if ($path -ne $null)
-#     {
-#         $newItems = $path.Split(';', [StringSplitOptions]::RemoveEmptyEntries) | Where-Object { "$($_)" -inotlike $VariableToRemove }
-#         [Environment]::SetEnvironmentVariable("PATH", [System.String]::Join(';', $newItems), "User")
-#     }
-#
-#     $path = [Environment]::GetEnvironmentVariable("PATH", "Process")
-#     if ($path -ne $null)
-#     {
-#         $newItems = $path.Split(';', [StringSplitOptions]::RemoveEmptyEntries) | Where-Object { "$($_)" -inotlike $VariableToRemove }
-#         [Environment]::SetEnvironmentVariable("PATH", [System.String]::Join(';', $newItems), "Process")
-#     }
-# }
-
-# Get .NET Core CLI path if installed.
-# $FoundDotNetCliVersion = $null;
-# if (Get-Command dotnet -ErrorAction SilentlyContinue) {
-#     $FoundDotNetCliVersion = dotnet --version;
-# }
-#
-# if($FoundDotNetCliVersion -ne $DotNetVersion) {
-#     $InstallPath = Join-Path $PSScriptRoot ".dotnet"
-#     if (!(Test-Path $InstallPath)) {
-#         mkdir -Force $InstallPath | Out-Null;
-#     }
-#
-#     (New-Object System.Net.WebClient).DownloadFile($DotNetInstallerUri, "$InstallPath\dotnet-install.ps1");
-#     & $InstallPath\dotnet-install.ps1 -Channel Current -Version $DotNetVersion -InstallDir $InstallPath;
-#
-#     Remove-PathVariable "$InstallPath"
-#     $env:PATH = "$InstallPath;$env:PATH"
-#     $env:DOTNET_SKIP_FIRST_TIME_EXPERIENCE=1
-#     $env:DOTNET_CLI_TELEMETRY_OPTOUT=1
-#
-#     & dotnet --info
-# }
 
 ###########################################################################
 # INSTALL CAKE
